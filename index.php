6d46483b90aec8266114dd24ac4e957a8d2c7c22
diaquino <25682@ma-web.nl>
2020-01-29 22:20:55 +0100

    Navbar en styling

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SecuredFood</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<div class="smartphone">
+  <div class="content">
+    
+  <div class="navbar">
+  <a class="navbar__menu-item" href="#"><img class="navbar__menu-item__logo" src="img/logo.png" alt="logo"></a>
+  <a class="navbar__menu-item navbar__menu-item__search" href="#">Contactweg, Amsterdam</a>
+  <div class="dropdown">
+  <button class="dropbtn"><img class="navbar__menu-item__menu-logo" onclick="myFunction()" src="img/menu.png" alt="menu"></button>
+  <div class="dropdown-content" id="myDropdown">
+    <a href="#">Shopping-cart</a>
+    <a href="#">Log-in</a>
+  </div>
+  </div> 
+</div>
+
+  </div>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
