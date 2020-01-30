6ddc788377af3f0e05da5e89aec9142c22b44488
diaquino <25682@ma-web.nl>
2020-01-30 09:05:39 +0100

    font added, navbar seperated

---
+++ template/homepage.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="../css/homepage.css">
+
+<?php 
+include 'navbar.php';
+?>
+
+<div class="header">
+    <img class="header__image" src="../img/steak.jpg" alt="header image">
+    <div class="header__image-div"></div>
+</div>
+
+<div class="category">
+<span class="category-item active">All</span> 
+<span class="category-item">Hamburgers</span> 
+<span class="category-item">Fish</span> 
+<span class="category-item">Chinese</span> 
+<span class="category-item">Mexican</span> 
+<span class="category-item">It</span>
+</div>
+
+<div class="meal-item">
+</div>
+    
+  </div>
+</div>
+
+<script src="../js/script.js"></script>
